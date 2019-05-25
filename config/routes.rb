4efb8455f812089f1b9802f2aef0b760a4b2f56b
sean <smgelbgithub@gmail.com>
2019-05-24 23:53:31 -0400

    4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { sessions: 'new_header' }
   
   as :user do
     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
