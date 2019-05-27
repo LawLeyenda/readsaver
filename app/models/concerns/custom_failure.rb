93ebc27a1ced1a72d02455b5ed4d08d6f748de30
sean <smgelbgithub@gmail.com>
2019-05-27 08:13:02 -0400

    deleted respond method in customfailure.rb

--- app/models/concerns/custom_failure.rb
+++ app/models/concerns/custom_failure.rb
@@ -1,12 +1,6 @@
 class CustomFailure < Devise::FailureApp
   def redirect_url
     redirect_to login_path
-end
-  def respond
-    if http_auth?
-      http_auth
-    else
-      redirect
-    end
   end
+  
 end
